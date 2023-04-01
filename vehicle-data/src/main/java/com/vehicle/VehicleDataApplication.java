5fe3f025ccd48312a03fcced759130670fb2050b
vinayak kamati <vinayakkamati123@gmail.com>
2023-04-01 15:31:20 +0530

    Created project repo

---
+++ vehicle-data/src/main/java/com/vehicle/VehicleDataApplication.java
@@ -0,0 +1,13 @@
+package com.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDataApplication.class, args);
+	}
+
+}
